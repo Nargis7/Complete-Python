b58aa50905f1488fe959dc70d8c6bd61ea564244
nargis7 <ronaldonargis786@gmail.com>
2026-05-26 16:43:00 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello , we are learning python!")
+
+# comment
+""" multiple line comment """ 
+
+# variables
+name = "Nargis"
+age = 21
+print(name)
+print(age)
+
+# naming convention
+NargisPerween = "student" # pascal case
+nargisPerween = "nargis"  # camel case
+nargis_perween = "nargis" # snake case
